294827055141f7401e87864f95254b2eb43b62a8
RuRuTiaSaMa <1009087450@qq.com>
2019-11-24 16:46:54 +0800

    get blobs from my own dir
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-# Copyright (C) 2019 The LineageOS Open Source Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,43 +18,34 @@
 
 set -e
 
+DEVICE_COMMON=msm8998-common
+VENDOR=nubia
+
 INITIAL_COPYRIGHT_YEAR=2019
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-ROM_ROOT="${MY_DIR}/../../.."
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="${ROM_ROOT}"/buildtools/extract_utils.sh
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
     exit 1
 fi
-source "${HELPER}"
+. "$HELPER"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ROM_ROOT}" true
+# Initialize the helper
+setup_vendor "${DEVICE_COMMON}" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "nx563j nx595j"
+write_headers "nx563j nx595j nx609j"
 
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+write_makefiles "$MY_DIR"/proprietary-files.txt true
+
+cat << EOF >> "$ANDROIDMK"
+EOF
 
 # Finish
 write_footers
-
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ROM_ROOT}" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi
